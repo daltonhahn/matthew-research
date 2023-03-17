4c9315aec7899fb7f1b62925bb079b8055705dca
Matthew <mpetillo@ku.edu>
2023-03-17 16:27:11 -0500

    creating main_tree.py and modifying functions.py to switch to binary node tree method

--- OPA-Testing/main_tree.py
+++ OPA-Testing/main_tree.py
@@ -4,6 +4,7 @@ import networkx as nx
 def main():
     num = int(input("How many nodes would you like created?: "))
     graph = f.createRandomNodeGraph(num)
+    print(nx.forest_str(graph))
     jsondict = f.getjson(graph)
     nodelist = f.sortjson(jsondict)
     f.uploadmicroserviceshells(nodelist)
